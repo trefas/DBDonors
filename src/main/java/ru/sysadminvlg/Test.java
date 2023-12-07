ab01fd9a970ca3bafefa3707f30c4d07a4f82f34
trefas <aismnw@gmail.com>
2023-12-07 11:45:15 +0300

    day 7

--- src/main/java/ru/sysadminvlg/Test.java
+++ src/main/java/ru/sysadminvlg/Test.java
@@ -4,6 +4,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.time.LocalDate;
+
 public class Test extends Application {
     @Override
     public void start(Stage stage) throws Exception {
